53efb898b53ecfb508d349ad2c119f361583938d
Marvin-desmond <marvindesmond@gmail.com>
2024-07-26 19:21:49 -0400

    create imagine implement

--- src/Controller/ImaginesController.ts
+++ src/Controller/ImaginesController.ts
@@ -10,7 +10,43 @@ export default class ImaginesController {
     static async Init(client: any ) {
         ImaginesRepository.Init(client)
     }
-
+    static async CreateImagine(req: Request, res: Response):Promise<Response<ApiResponse>>{
+        try {
+            let id: string | undefined = req.params.id
+            if (!id) {
+                return res.send({
+                    satus: 500,
+                    data: null,
+                    message: "poem id parameter not passed"
+                })
+            }
+            let _id = new ObjectId(id)
+            let model = genAIModel()
+            let result = await PoemsRepository.GetPoem(_id)
+            if (!result) {
+                return res.send({
+                    status: 204,
+                    data:null, 
+                    message: "poem not found"
+                })
+            }
+            let prompt = genAIPrompt(result.poem),
+                content = await model.generateContent(prompt),
+                generated_prompt = content.response.text();
+                await ImaginesRepository.CreateImagine(_id, generated_prompt)
+            return res.send({
+                status: 200, 
+                data: `${generated_prompt}<EOS>`,
+                message: "poem imagination complete..."
+            })
+        } catch(e) {
+            return res.send({
+                status: 500, 
+                data: null,
+                message: "tired of imaginations..."
+            })
+        }
+    }
     static async GetImagines(req: Request, res: Response):Promise<Response<ApiResponse>>{
         try {
             let imagines = (await ImaginesRepository.GetImagines()) as Imagine[]
@@ -27,7 +63,6 @@ export default class ImaginesController {
             })
         }
     }
-
     static async UpdateImagine(req: Request, res: Response):Promise<Response<ApiResponse>>{
         try {
             let id: string | undefined = req.params.id
@@ -51,7 +86,7 @@ export default class ImaginesController {
             let prompt = genAIPrompt(result.poem),
                 content = await model.generateContent(prompt),
                 generated_prompt = content.response.text();
-            let result_imagine = await ImaginesRepository.CreateImagine(_id, generated_prompt)
+            let result_imagine = await ImaginesRepository.UpdateImagine(_id, generated_prompt)
             return res.send({
                 status: 200, 
                 data: result_imagine,
