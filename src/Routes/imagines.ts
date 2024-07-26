fb9b4706007d0aeabd3d1a58c2097f4a3adea0ae
Marvin-desmond <marvindesmond@gmail.com>
2024-07-26 19:19:43 -0400

    create imagine route

--- src/Routes/imagines.ts
+++ src/Routes/imagines.ts
@@ -4,6 +4,7 @@ import ImaginesController from "../Controller/ImaginesController"
 
 const router = express.Router()
 
+router.post("/create/:id", ImaginesController.CreateImagine)
 router.put("/update/:id", ImaginesController.UpdateImagine)
 router.get("/", ImaginesController.GetImagines)
 
