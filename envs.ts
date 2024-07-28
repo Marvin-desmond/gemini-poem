c82d8bc7f6d82a7769b9f7906ba50b4db0eca1a5
Marvin-desmond <marvindesmond@gmail.com>
2024-07-28 02:21:00 -0400

    add env name for stability_api

--- envs.ts
+++ envs.ts
@@ -4,6 +4,7 @@ export const ENVS = {
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
     PORT: process.env.port || 8080,
     MONGODB_URI: process.env.MONGODB_URI,
-    DB_NAME: process.env.DB_NAME
+    DB_NAME: process.env.DB_NAME,
+    STABILITY_API_KEY: process.env.STABILITY_API_KEY
   }
 
