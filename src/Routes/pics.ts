2a5e94adb2955b5695c9dc2e7f1240e954ce5b85
Marvin-desmond <marvindesmond@gmail.com>
2024-08-01 23:08:42 -0400

    get metadata and update pic routes

--- src/Routes/pics.ts
+++ src/Routes/pics.ts
@@ -5,7 +5,9 @@ import PicsController from "../Controller/PicsController"
 const router = express.Router()
 
 router.get("/get/:id", PicsController.GetPic)
+router.get("/getmeta", PicsController.GetAllPicMeta)
 router.post("/create", PicsController.CreatePic)
+router.put("/update", PicsController.UpdatePic)
 router.delete("/delete/:id", PicsController.DeletePic)
 
 export default router
