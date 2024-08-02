9be679fa0615c511555857a2829738fdb558c7e1
Marvin-desmond <marvindesmond@gmail.com>
2024-08-01 23:18:17 -0400

    implementing getting pic metadata, getting all pic metadata

--- src/Controller/PicsController.ts
+++ src/Controller/PicsController.ts
@@ -1,28 +1,28 @@
 import { Request, Response } from "express"
-import { ApiResponse, UploadResponse } from "../../types"
+import { ApiResponse, PicMeta, UploadResponse } from "../../types"
 import { ObjectId } from "mongodb"
 import fs from "fs"
 
 import PicsRepository from "../Repository/PicsRepository"
-import ImaginesRepository from "../Repository/ImaginesRepository"
-import { genAIModel, genAIPrompt } from "../../genAI"
 
 export default class PicsController {
     static async Init(client: any ) {
         PicsRepository.Init(client)
     }
     static async CreatePic(req: Request, res: Response):Promise<Response<ApiResponse>>{
-        let prompt = req.body?.prompt
-        if (!prompt) {
+        let poem_id: string | undefined = req.body?.poem_id
+        let prompt: string | undefined = req.body?.prompt
+        if (!prompt || !poem_id) {
             return res.send({
-                satus: 500,
+                status: 500,
                 data: null,
-                message: "prompt err"
+                message: "prompt or poem_id err"
             })
         }
         try {
-            let result = await PicsRepository.CreatePic(prompt)
-            if (result.done) {
+            let _poem_id = new ObjectId(poem_id)
+            let result = await PicsRepository.CreatePic(_poem_id, prompt)
+            if (result.stream_done) {
                 return res.send({
                     status: 200, 
                     data: result,
@@ -47,7 +47,7 @@ export default class PicsController {
         let id = req.params.id
         if (!id) {
             return res.send({
-                satus: 500,
+                status: 500,
                 data: null,
                 message: "file_id parameter err"
             })
@@ -55,7 +55,7 @@ export default class PicsController {
         try {
             let _id = new ObjectId(id)
             let result = (await PicsRepository.GetPic(_id)) as UploadResponse
-            fs.writeFileSync("gotten.png", result.buffer!)
+            // fs.writeFileSync("gotten.png", result.buffer!)
             result.buffer = Buffer.from(result.buffer!).toString('base64')
             return res.send({
                 status: 200, 
@@ -70,11 +70,99 @@ export default class PicsController {
             })
         }
     }
+    static async GetPicMeta(req: Request, res: Response):Promise<Response<ApiResponse>> {
+        let id = req.params.id
+        if (!id) {
+            return res.send({
+                status: 500,
+                data: null,
+                message: "poem_id parameter err"
+            })
+        }
+        try {
+            let _id = new ObjectId(id)
+            let result = await PicsRepository.GetPicMeta(_id)
+            if (result) {
+                return res.send({
+                    status: 200, 
+                    data: result,
+                    message: "pics meta fetched"
+                })
+            } else {
+                return res.send({
+                    status: 204,
+                    data:null, 
+                    message: "pics meta not found"
+                })
+            }
+        } catch(e) {
+            return res.send({
+                status: 500, 
+                data: null,
+                message: "pics meta fetch err"
+            })
+        }
+    }
+    static async GetAllPicMeta(req: Request, res: Response):Promise<Response<ApiResponse>>{
+        
+        try {
+            let allpicsmeta = (await PicsRepository.GetAllPicMeta()) as PicMeta[]
+            console.log(allpicsmeta);
+            
+            return res.send({
+                status: 200, 
+                data: allpicsmeta,
+                message: "pic metadata fetched"
+            })
+        } catch(e) {
+            console.log(e)
+            return res.send({
+                status: 500, 
+                data: null,
+                message: "pic metadata fetch err"
+            })
+        }
+    }
+    static async UpdatePic(req: Request, res: Response){
+        let file_id: string | undefined = req.body.file_id
+        let generated_prompt: string | undefined = req.body.prompt
+        if (!file_id || !generated_prompt) {
+            return res.send({
+                status: 500,
+                data: null,
+                message: "file_id prompt parameter err"
+            })
+        }
+        try { 
+            let _file_id = new ObjectId(file_id)
+            await PicsRepository.DeletePic(_file_id)
+            let result = await PicsRepository.UpdatePic(_file_id, generated_prompt)
+            if (result.stream_done) {
+                return res.send({
+                    status: 200, 
+                    data: result,
+                    message: "pic updated"
+                })
+            } else {
+                return res.send({
+                    status: 500, 
+                    data: result,
+                    message: "pic udate exec err"
+                })
+            }
+        } catch(e) {
+            return res.send({
+                status: 500, 
+                data: null,
+                message: "pic update err"
+            })
+        }
+    }
     static async DeletePic(req: Request, res: Response){
         let id = req.params.id
         if (!id) {
             return res.send({
-                satus: 500,
+                status: 500,
                 data: null,
                 message: "file_id parameter err"
             })
@@ -91,7 +179,7 @@ export default class PicsController {
             // console.log(Object.getOwnPropertyNames(e))
             console.log((e as unknown as Error).message)
             return res.send({
-                satus: 500,
+                status: 500,
                 data: null,
                 message: "pic delete err"
             })
