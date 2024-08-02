b81f87ff690ef9eb6edc75dd7514d1efa2b4d9fa
Marvin-desmond <marvindesmond@gmail.com>
2024-08-01 23:09:17 -0400

    delete imagine route

--- src/Routes/imagines.ts
+++ src/Routes/imagines.ts
@@ -6,6 +6,7 @@ const router = express.Router()
 
 router.post("/create/:id", ImaginesController.CreateImagine)
 router.put("/update/:id", ImaginesController.UpdateImagine)
+router.delete("/delete/:id", ImaginesController.DeleteImagine)
 router.get("/", ImaginesController.GetImagines)
 
 export default router
