24e15817435b207989c46b4ddde5d247aa050784
Marvin-desmond <marvindesmond@gmail.com>
2024-08-01 23:12:55 -0400

    updatePic in bucket implemented, init now bundles bucket and pic metadata collection, modularize getting prompt as function

--- src/Repository/PicsRepository.ts
+++ src/Repository/PicsRepository.ts
@@ -1,26 +1,35 @@
-import { ObjectId, GridFSBucket } from "mongodb"
+import { ObjectId, GridFSBucket, Collection } from "mongodb"
 import { Readable, Writable } from "node:stream"
 import { pipeline } from 'node:stream/promises'
 import assert from 'assert'
 
-import { UploadResponse } from "../../types"
+import { PicMeta, UploadResponse } from "../../types"
 import { stableImage } from "../../generateImage"
 import { generateSlug } from "random-word-slugs"
 
 export default class PicsRepository {
+    static picsMeta?: Collection
     static bucket?: GridFSBucket
     static async Init(client_db: any ) {
-        if (this.bucket) return
-        this.bucket = new GridFSBucket(client_db)
+        if (this.bucket && this.picsMeta) return
+        try {
+            this.bucket = new GridFSBucket(client_db)
+            this.picsMeta = client_db.collection("picsMeta")            
+        } catch (e) {
+            console.error(
+              `pics repository init fail: ${e}`,
+            )
+          }
     }
 
-    static async CreatePic(generated_prompt: string): Promise<UploadResponse>{
+    static async CreatePic(poem_id: ObjectId, generated_prompt: string): Promise<UploadResponse>{
         const result: UploadResponse = {
-            done: false,
+            stream_done: false,
+            meta_done: false,
             file_id: undefined,
             buffer: undefined
         }
-        const response = await stableImage(generated_prompt.slice(18, -2))
+        const response = await stableImage(generated_prompt)
         let buffer: Buffer
         let stream_file_name = generateSlug()
         if (response.status === 200) {
@@ -40,20 +49,26 @@ export default class PicsRepository {
             upload_stream
         )
         result.file_id = upload_stream.id
-        result.done = true
+        result.stream_done = true
+        let result_meta_save = await this.picsMeta!.insertOne({ 
+            poem_id: poem_id,
+            file_id: result.file_id
+        })
+        result.meta_done = result_meta_save.acknowledged
         return result
     }
 
     static async GetPic(file_id: ObjectId):Promise<UploadResponse> {
         const result: UploadResponse = {
-            done: false,
+            stream_done: false,
+            meta_done: false,
             file_id: undefined,
             buffer: undefined
         }
         let buffers: any = []
         const bufferStream = new Writable({
             write(chunk, encoding, callback) {
-              buffers.push(chunk);
+              buffers.push(chunk)
               callback();
             }
           })
@@ -62,13 +77,51 @@ export default class PicsRepository {
             this.bucket!.openDownloadStream(file_id),
             bufferStream
         )
-        result.done = true;
+        result.stream_done = true;
         result.buffer = Buffer.concat(buffers)
         return result
     }
 
-    static async UpdatePic(file_id: ObjectId, generated_prompt: string){
+    static async GetAllPicMeta(): Promise<PicMeta[]> {
+        // @ts-ignore
+        let allPicsMeta = (await this.picsMeta!.find({}, {_id: 0})).toArray() as PicMeta[]
+        return allPicsMeta
+    }
 
+    static async GetPicMeta(poem_id: ObjectId):Promise<PicMeta> {
+        // @ts-ignore
+        let picsMeta = (await this.picsMeta!.findOne({ poem_id: poem_id }, { _id: 0 })) as PicMeta
+        return picsMeta
+    }
+
+    static async UpdatePic(file_id: ObjectId, generated_prompt: string){
+        const result: UploadResponse = {
+            stream_done: false,
+            meta_done: false,
+            file_id: undefined,
+            buffer: undefined
+        }
+        const response = await stableImage(generated_prompt)
+        let buffer: Buffer
+        let stream_file_name = generateSlug()
+        if (response.status === 200) {
+            buffer = Buffer.from(response.data)
+        } else {
+            throw new Error(`error generating buffer`)
+        }
+        const stream = new Readable({
+            read() {
+                this.push(buffer);
+                this.push(null);
+            }
+        })
+        const upload_stream = this.bucket!.openUploadStreamWithId(file_id, `${stream_file_name}.png`)
+        await pipeline(
+            stream,
+            upload_stream
+        )
+        result.stream_done = true
+        return result
     }
     static async DeletePic(file_id: ObjectId):Promise<void>{
         await this.bucket!.delete(file_id)
