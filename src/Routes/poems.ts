f3793e8d140ea1f54c39b2e6960d69454a0e4034
Marvin-desmond <marvindesmond@gmail.com>
2024-08-05 01:02:48 -0400

    routes for poem retrieval from photos and videos

--- src/Routes/poems.ts
+++ src/Routes/poems.ts
@@ -3,12 +3,18 @@ import PoemsController from "../Controller/PoemsController"
 
 const router = express.Router()
 
+import multer from "multer"
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
 router.post("/create", PoemsController.CreatePoem)
 router.put("/update", PoemsController.UpdatePoem)
 router.get("/get/:id", PoemsController.GetPoem)
 router.get("/", PoemsController.GetPoems)
 router.delete("/delete/:id", PoemsController.DeletePoem)
-
+router.post("/extract", upload.single('image'), PoemsController.PoemFromFile)
+router.post("/extractvideo", upload.single('video'), PoemsController.PoemFromVideo)
 export default router 
 
 // router.route("/").get(PoemsController.GetPoems)
