72119fe49355f51de162d16f066122415a1cfa7b
Marvin-desmond <marvindesmond@gmail.com>
2024-08-10 18:42:34 -0400

    clear comments and unused imports

--- generateImage.ts
+++ generateImage.ts
@@ -1,13 +1,6 @@
-import fs from "node:fs";
 import axios, { AxiosResponse } from "axios";
 import FormData from "form-data"
-import connection from "./db"
-
-import { Readable } from "node:stream"
-import { GridFSBucket } from "mongodb"
 import { ENVS } from "./envs"
-import { generateSlug } from "random-word-slugs"
-import assert from 'assert'
 
 const stableTextToImagePayload = (prompt: string): any => {
   return {
@@ -27,44 +20,6 @@ const stableVersion = (version?: string):string => {
     return versions[_version]
 }
 
-/*
-connection().then(async client => {
-    const prompt = `A colossal, crimson bull, its horns tipped with molten gold, stands defiant against a backdrop of rolling, 
-    snow-capped hills. The bull's eyes burn with an otherworldly fire, reflecting the stark, white expanse of the farmer's 
-    land. Rendered in a hyperrealistic, video game animation style with cinematic lighting, emphasizing the dramatic contrasts 
-    of light and shadow. The scene evokes a sense of awe and unease, as if witnessing a mythical creature from a forgotten age`
-    try {
-    const _client = client.db(ENVS.DB_NAME)
-    var bucket = new GridFSBucket(_client)
-    const response = await stableImage(prompt)
-    let buffer: Buffer
-    if (response.status === 200) {
-        const word = generateSlug()
-        fs.writeFileSync(`./public/random/${word}.png`, Buffer.from(response.data))
-        buffer = Buffer.from(response.data)
-      } else {
-        console.log(`error generating buffer`)
-        return
-      }
-    const stream = new Readable()
-    const upload_stream = bucket.openUploadStream('meistersinger.png')
-    stream.push(buffer)
-    stream.push(null)
-    stream.pipe(upload_stream).
-    on('error', function(error) {
-      assert.ifError(error);
-    }).
-    on('finish', function() {
-        console.log('File ID:', upload_stream.id)
-      console.log('done!');
-      process.exit(0);
-    })
-    } catch (e) {
-      console.error(`Unable to establish connection ${e}`)
-    }
-  })
-*/
-
 export const stableImage = async (prompt: string):Promise<AxiosResponse<any, any>> => {
   const payload = stableTextToImagePayload(prompt)
   const stable_version = stableVersion()
@@ -81,11 +36,4 @@ export const stableImage = async (prompt: string):Promise<AxiosResponse<any, any
     }
   );
   return response
-//   if (response.status === 200) {
-//     const word = generateSlug()
-//     fs.writeFileSync(`./public/random/${word}.png`, Buffer.from(response.data))
-//     return Buffer.from(response.data)
-//   } else {
-//     throw new Error(`${response.status}: ${response.data.toString()}`);
-//   }
 }
